2438cfba97bb3f3da92ab229c556f268be24d9a1
Himanshu <169117364+imphenomenal07@users.noreply.github.com>
2026-03-24 13:47:03 +0530

    6️. Find largest of 3 numbers

---
+++ basic-level/Level 1/task6.py
@@ -0,0 +1,14 @@
+# 6️. Find largest of 3 numbers
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    print(f"{num1} is greater than {num2} and {num3}")
+
+elif num2 >= num1 and num2 >= num3:
+    print(f"{num2} is greater than {num1} and {num3}")
+
+else:
+    print(f"{num3} is greater than {num1} and {num2}")
