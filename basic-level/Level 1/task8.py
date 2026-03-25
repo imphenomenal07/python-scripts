e153fed4d050da847d867940d297b210f1489a45
Himanshu <169117364+imphenomenal07@users.noreply.github.com>
2026-03-25 12:25:07 +0530

    8️. Calculate simple interest

---
+++ basic-level/Level 1/task8.py
@@ -0,0 +1,17 @@
+#8️. Calculate simple interest
+
+#Formula: (P * R * T) / 100
+
+p = int(input("enter Principle: "))
+
+r = int(input("enter Rate: "))
+
+t = int(input("enter Time(in years) : "))
+
+interest = p*r*t/100
+
+print("Simple interest is", interest)
+
+amt = p + interest
+
+print("Total amount is", amt)
