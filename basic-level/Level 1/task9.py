dfd515abd9638b564ff9b1f15594f943f9603505
Himanshu <169117364+imphenomenal07@users.noreply.github.com>
2026-03-26 14:03:21 +0530

    9️. Convert Celsius to Fahrenheit

---
+++ basic-level/Level 1/task9.py
@@ -0,0 +1,7 @@
+# 9️. Convert Celsius to Fahrenheit
+
+celcius = float(input("Enter the temperature(in celcius): "))
+
+fahrenheit = (celcius*9/5)+32
+
+print(f"Temperature in Fahrenheit:{fahrenheit}")
